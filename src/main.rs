6aa48747d52cd89f1b7f455d4fcccf07a786a75b
CharlesMassuard <charles.massuard@etu.univ-orleans.fr>
2025-01-09 11:46:05 +0100

    Questions 2&3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // ouvrir image
+    let img = image::open("../imgs/stark.png").unwrap();
+
+   
+    // convertir l'image en mode RGB8
+    let rgb_img = img.to_rgb8();
+
+    // sauvegarder au format PNG
+    rgb_img.save("../imgs/stark_rgb.png").unwrap();
 }
