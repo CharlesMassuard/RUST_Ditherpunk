287329f8f73a50c50a00fc86b7732a91889fffaa
CharlesMassuard <charles.massuard@etu.univ-orleans.fr>
2025-01-09 12:09:34 +0100

    Question 4

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,11 @@ fn main() {
 
     // sauvegarder au format PNG
     rgb_img.save("../imgs/stark_rgb.png").unwrap();
+
+    afficher_couleurs_pixel(&rgb_img, 32, 52);
+}
+
+fn afficher_couleurs_pixel(img: &image::RgbImage, x: u32, y: u32) {
+    let pixel = img.get_pixel(x, y);
+    println!("Les couleurs du pixel ({}, {}) sont : {:?}", x, y, pixel);
 }
